d628e1f4699b2260a92255d229f7096e9a0a0b7c
Humphrey Ahn <ahnsv@bc.edu>
2019-04-14 10:50:07 +0900

    fix: CSS bundling webpack done, respective image done TODO: UI, ladder logic

--- templates/static/webpack.config.js
+++ templates/static/webpack.config.js
@@ -4,9 +4,8 @@ const config = {
     devtool: 'eval-source-map',
     entry: __dirname + '/js/index.js',
     output: {
-        path: resolve('../public'),
-        filename: 'bundle.js',
-        publicPath: resolve('../public')
+        path: __dirname + '/../public',
+        filename: 'bundle.js'
     },
     resolve: {
         extensions: ['.js', '.jsx', '.css']
